4192ae5dba870343c2086182b133114c3f05fbfb
Dmitry M <matovxd@gmail.com>
2023-05-05 17:06:46 +0300

    create trip_info layout

--- app/src/main/java/com/example/businesstripsapp/trips_history_activity/recycler_view/TripHistoryViewHolder.kt
+++ app/src/main/java/com/example/businesstripsapp/trips_history_activity/recycler_view/TripHistoryViewHolder.kt
@@ -3,15 +3,15 @@ package com.example.businesstripsapp.trips_history_activity.recycler_view
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.businesstripsapp.databinding.TripHistoryListItemBinding
-import com.example.businesstripsapp.trips_activity.models.Trip
+import com.example.businesstripsapp.trips_history_activity.models.Trip
 
 class TripHistoryViewHolder(item: View): RecyclerView.ViewHolder(item) {
     val binding = TripHistoryListItemBinding.bind(item)
 
     fun bind(trip: Trip, listener: TripsHistoryAdapter.Listener) {
         binding.tripId.text = "№" + trip.id
-        binding.destinationCity.text = trip.destinationCity
-        binding.tripDate.text = trip.tripDate
+        binding.destinationCity.text = trip.destination.office.address
+        binding.tripDate.text = trip.startDate + " - " + trip.endDate
 
         itemView.setOnClickListener {
             listener.onClick(trip)
