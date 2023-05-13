fba50d66ea20e9037898ce4572026531a9686ca9
Dmitry M <matovxd@gmail.com>
2023-05-13 22:37:02 +0300

    rebase main

--- app/src/main/java/com/example/businesstripsapp/trips_activity/domain/tripApi.kt
+++ app/src/main/java/com/example/businesstripsapp/trips_activity/domain/tripApi.kt
@@ -1,12 +1,9 @@
 package com.example.businesstripsapp.trips_activity.domain
 
-import com.example.businesstripsapp.trips_activity.models.Accommodation
 import com.example.businesstripsapp.trips_activity.models.Trip
 import io.reactivex.Single
 import retrofit2.Response
-import retrofit2.http.Body
 import retrofit2.http.GET
-import retrofit2.http.POST
 import retrofit2.http.Path
 
 const val TRIP_URL: String = "/users/{uuid}/trips-at/1"
